9377892c199c8f206f1aaeffc0f8f6a79cc44e15
acxz <17132214+acxz@users.noreply.github.com>
2020-12-08 22:51:04 -0500

    add gpytorch dep

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=[
         'argparse',
+        'gpytorch',
         'pytorch-lightning',
         'torch',
     ],
